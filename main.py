c56357b479cf1467c518000888b61493b5a427af
prajwalgh <76061744+prajwalgh@users.noreply.github.com>
2022-12-03 19:45:03 +0530

    new file

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-#TODO print element from li tag and a tag inside a div and create a dictionary
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 
 chrome_driver_path="C:\devlopmentAppBr\chromedriver.exe"
 driver=webdriver.Chrome(executable_path=chrome_driver_path)
-
 
 driver.get("https://www.python.org/")#[@id="content"]div/section/div[3]/div[2]/div/ul/li[1]/time/text()
 GetTime=driver.find_elements(By.XPATH,'//*[@id="content"]/div/section/div[3]/div[2]/div/ul/li/time')
